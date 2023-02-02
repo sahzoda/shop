e0da6d63b9765a9ef1a71b005707ba89c0f930c0
sahzoda <shahzodanigmatjanova524@gmail.com>
2023-02-02 17:57:06 +0600

    animation page

---
+++ lib/utilities/animation_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AnimationColor{
+  static const List<Color> animationcolor=[
+    Colors.black,
+    Colors.white,
+    Colors.yellow,
+    Colors.blue,
+    Colors.grey,
+    Colors.lime,
+    Colors.green,
+  ];
+}
