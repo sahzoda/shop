954019f122eb1fa62399a6fde257fa3e5adb747c
sahzoda <shahzodanigmatjanova524@gmail.com>
2023-02-09 17:55:08 +0600

    firebase started

--- lib/pages/welcome_page/welcome_page.dart
+++ lib/pages/welcome_page/welcome_page.dart
@@ -13,6 +13,18 @@ const WelcomePage({ Key? key }) : super(key: key);
             image:AssetImage("assets/images/imapp/bgimage.jpg")                 
           )
         ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child:Column(
+            children: [
+              SizedBox(height: 120,
+              child: Image(
+                image: AssetImage("assets/images/inapp/logo.jpg")
+                ),
+                ),
+            ]
+            ) ,
+        ),
       ),
     );
   }
